e009a3c565ef1400e465a3a30473982ca4408fef
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2014-05-29 15:00:42 +0700

    define set_article in articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, except: [:index, :new]
+
   def new
   	@article = Article.new
   end
@@ -8,8 +10,6 @@ class ArticlesController < ApplicationController
   end
   
   def create
-  	@article = Article.new(article_params)
-  	
   	if @article.save
   		redirect_to @article
   	else
@@ -18,16 +18,12 @@ class ArticlesController < ApplicationController
   end
   
   def show
-  	@article = Article.find(params[:id])
   end
   
   def edit
-  	@article = Article.find(params[:id])
   end
   
   def update
-  	@article = Article.find(params[:id])
-  	
   	if @article.update(article_params)
   		redirect_to @article
   	else
@@ -36,7 +32,6 @@ class ArticlesController < ApplicationController
   end	
   
   def destroy
-  	@article = Article.find(params[:id])
   	@article.destroy
   	
   	redirect_to articles_path
@@ -44,6 +39,10 @@ class ArticlesController < ApplicationController
   
   
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
   	def article_params
   		params.require(:article).permit(:title, :text)
   	end
