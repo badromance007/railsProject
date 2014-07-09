851c4be527911c800351a99aad38fa539137e9b3
Tuong Nguyen <nguyentrongtuong1993@yahoo.com>
2014-07-09 17:42:19 +0700

    setup for Heroku

---
+++ cms_monnhat/monnhat_plus/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonnhatPlus::Application.config.secret_token = '8a707f5e21816ad089450ac43b7f7c12ab4e184c3d394c1c28afbe0664e100cc02b61f9748e2d1b33807a9cd2a1d11f27140de950e78788504f7488302ddf311'
